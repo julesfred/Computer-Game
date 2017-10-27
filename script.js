79cf8c4dbac0e4fdc053f7bd63082b5defc93533
julesfred <jules.tf4@yahoo.com>
2017-10-26 23:00:33 -0400

    cleaned up javascript

--- script.js
+++ script.js
@@ -15,25 +15,25 @@ var playing = true;
 
 while (playing) {
     if (wins === 3) {
-    console.log("Congratulations! You won the game.");
-    break;
-}   
+        console.log("Congratulations! You won the game.");
+        break;
+    }   
 
     health -= Math.floor((Math.random() * 2) + 1);
     enemyHealth -= Math.floor((Math.random() * 2) + 1);
     console.log(name + ", your health is " + health);
     console.log("Grant's health is " + enemyHealth);
 
-if (enemyHealth <= 0) {
-    wins++;
-    enemyHealth = 10;
-    console.log(name + ", you won a battle!!!");
+    if (enemyHealth <= 0) {
+        wins++;
+        enemyHealth = 10;
+        console.log(name + ", you won a battle!!!");
     }
 
-if (health <=0 || enemyHealth <=0) {
-    console.log("Sorry you lost, the game is over.");
-    playing = false;
-}
+    if (health <=0 || enemyHealth <=0) {
+        console.log("Sorry you lost, the game is over.");
+        playing = false;
+    }
 
 } /*Lab 1 out of 4*/
 
